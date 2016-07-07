7845ed7ba8111558b621ef716777e27ce4726317
AnhtuanUit <anhtuan7194@gmail.com>
2016-07-07 11:47:29 +0700

    v1 fiexed 4

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var exec = require('child_process');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var message = 80;
+
+	exec.execFile('./remote',
+		['-m', message],
+		function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if( stdout.indexOf("Got this response") > -1 ){
+				var state = stdout.split('Got this response ')[1].split('.')[0];
+				
+			}
+
+
+			if (error !== null) {
+				console.log('exec error: ' + error);
+
+				
+
+			}
+		});
+
+	res.send('respond with a resource');
 });
 
 module.exports = router;
